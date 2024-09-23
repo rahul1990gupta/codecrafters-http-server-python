3e7809ba2cb1f947fc624f2d2d45c019f006580e
Rahul Gupta <rahul1990gupta@gmail.com>
2024-09-23 09:54:57 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,11 +8,20 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, address = server_socket.accept() # wait for client
 
     # data = server_socket.recv(48)
-    message = b"HTTP/1.1 200 OK\r\n\r\n"
-    conn.send(message)
+    while True:
+        conn, address = server_socket.accept() # wait for client
+        
+        data = conn.recv(100)
+        req_parts = data.split(b"\r\n")
+        print("Request Line", req_parts[0])
+        url = req_parts[0].split(b" ")[1]
+        if url == b"/":
+            message = b"HTTP/1.1 200 OK\r\n\r\n"
+        else: 
+            message = b"HTTP/1.1 404 Not Found\r\n\r\n"
+        conn.send(message)
 
 
 
